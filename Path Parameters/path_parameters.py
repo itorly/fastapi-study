92482b8ac7b9d2b590be018a675a3b079cf764e8
itorly <itorly@163.com>
2025-07-31 23:22:53 +0800

    Path parameters with types

--- Path Parameters/path_parameters.py
+++ Path Parameters/path_parameters.py
@@ -7,3 +7,7 @@ app = FastAPI()
 async def read_item(item_id):
     return {"item_id": item_id}
 
+# Path parameters with types
+@app.get("/ints/{int_id}")
+async def read_int(int_id: int):
+    return {"int_id": int_id}
