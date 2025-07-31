91902ff66862d86548608b311d29ca535804841f
itorly <itorly@163.com>
2025-07-31 23:42:54 +0800

    Order matters

--- Path Parameters/path_parameters.py
+++ Path Parameters/path_parameters.py
@@ -11,3 +11,22 @@ async def read_item(item_id):
 @app.get("/ints/{int_id}")
 async def read_int(int_id: int):
     return {"int_id": int_id}
+
+# Order matters
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
+
+@app.get("/users")
+async def read_users():
+    return ["Rick", "Morty"]
+
+
+@app.get("/users")
+async def read_users2():
+    return ["Bean", "Elfo"]
