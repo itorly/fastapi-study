e5532c3f4a080e7f04e606c4828cc1ecd8b904eb
itorly <itorly@163.com>
2025-08-01 23:52:26 +0800

    Multiple path and query parameters

--- Query parameters/Query Parameters.py
+++ Query parameters/Query Parameters.py
@@ -29,3 +29,18 @@ async def read_item(item_id: str, q: str | None = None, short: bool = False):
             {"description": "This is an amazing item that has a long description"}
         )
     return item
+
+
+# Multiple path and query parameters
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: str | None = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
