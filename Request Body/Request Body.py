8196f81e4f97f7c6e2c78b0374740257615a8ca4
itorly <itorly@163.com>
2025-08-03 00:00:16 +0800

    Request body + path + query parameters

--- Request Body/Request Body.py
+++ Request Body/Request Body.py
@@ -21,3 +21,12 @@ async def create_item(item: Item):
 @app.put("/items/{item_id}")
 async def update_item(item_id: int, item: Item):
     return {"item_id": item_id, **item.dict()}
+
+
+# Request body + path + query parameters
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item, q: str | None = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
