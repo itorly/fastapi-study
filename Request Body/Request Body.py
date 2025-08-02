1dd3646d3c13d3a65c7864be6a2558b3404ff8d1
itorly <itorly@163.com>
2025-08-02 23:59:43 +0800

    Request body + path parameters

--- Request Body/Request Body.py
+++ Request Body/Request Body.py
@@ -15,3 +15,9 @@ app = FastAPI()
 @app.post("/items/")
 async def create_item(item: Item):
     return item
+
+
+# Request body + path parameters
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    return {"item_id": item_id, **item.dict()}
